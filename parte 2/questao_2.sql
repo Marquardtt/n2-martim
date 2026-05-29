e47acc3733f83ae9abbf52a9f14c3a2b93a75ff5
Gabriel Fellipe Marquardt <119713462+Marquardtt@users.noreply.github.com>
2026-05-29 19:16:31 -0300

    Add files via upload

---
+++ parte 2/questao_2.sql
@@ -0,0 +1,4 @@
+SELECT e.nome AS escola, COUNT(m.aluno_id) AS total_alunos FROM public.escolas e
+LEFT JOIN public.matriculas m ON m.escola_id = e.id
+GROUP BY e.id, e.nome
+ORDER BY total_alunos DESC;
